be2274005aac984ec878e3a8f221d17db9b9f219
norecha <norecha55@gmail.com>
2015-01-16 17:34:57 -0500

    main window stage is shown at the end of init now

--- pokubot/src/main/java/aok/coc/controller/UILauncher.java
+++ pokubot/src/main/java/aok/coc/controller/UILauncher.java
@@ -8,38 +8,22 @@ import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class UILauncher extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		Button btn = new Button();
-		btn.setText("Say 'Hello World'");
-		btn.setOnAction(e -> System.out.println(e.getSource()));
-		//		btn.setOnAction(new EventHandler<ActionEvent>() {
-		//
-		//			@Override
-		//			public void handle(ActionEvent event) {
-		//				System.out.println("Hello World!");
-		//			}
-		//		});
-
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
 
 		primaryStage.setTitle("PokuBot");
-		primaryStage.show();
 
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(UILauncher.class.getResource("/fxml/MainWindow.fxml"));
 		AnchorPane scene = loader.load();
 
 		primaryStage.setScene(new Scene(scene));
-
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
